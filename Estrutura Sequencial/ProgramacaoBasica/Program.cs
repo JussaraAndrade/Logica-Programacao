e895e592dd6881ffad604b6f06263f4708d53108
Jussara Andrade <jussara.jandrade1993@outlook.com>
2022-01-27 22:39:45 -0300

    variáveis e tipos básicos

---
+++ Estrutura Sequencial/ProgramacaoBasica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramacaoBasica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Estrutura Sequencial/ProgramacaoBasica/VariaveisTiposBasicos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramacaoBasica
+{
+    /*
+        - Variáveis - armazenar os dados na execução do programa
+         
+        Exemplos:
+            int idade = 25;
+            double altura = 1.68;
+            char sexo = 'F';
+
+        Obs: <tipo> <nome> = <valor inicial>;
+
+        Tipos básicos em C#: 
+        
+        Número Inteiros: byte, int, long, etc...
+        Ponto flutuante: float, double, decimal, char, string, bool e object
+
+        Camel Case (Nomes de variáveis):
+
+        int _5minutos; 
+        int salario;
+        int salarioDoFuncionario;
+     */
+    class VariaveisTiposBasicos
+    {
+       
+    }
+}
